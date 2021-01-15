42a27c0fecd29d997c041fb00696a62357210dfd
Jasper Voss <jaspervoss812@gmail.com>
2021-01-15 13:05:23 -0500

    a

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ snfile.close()
 
 turn = getturn()
 
-PORT = 5053
+PORT = 5054
 localIP = '192.168.1.21'
 globalIP = '71.232.76.201'
 localConnection = True    #Are both boards on home network?
@@ -141,6 +141,7 @@ while True:
 					moved_from[0] = i
 					moved_from[1] = j
 		if moved_from[0] != -1 and moved_from[1] != -1 and moved_to[0] != -1 and moved_to[1] != -1:
+			print(moved_from, moved_to)
 			connection.send(f'{moved_from[0]} {moved_from[1]} {moved_to[0]} {moved_to[1]}')
 			turn = 1-turn
 			state = new_state[:]
